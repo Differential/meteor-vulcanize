ae4a563ed12c9ebc79bcd9262c45265840386c89
schnie <greg@differential.com>
2015-01-05 08:23:25 -0500

    add git url

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   name: 'differential:vulcanize',
   summary: 'Vulcanize',
-  version: '0.0.1'
+  version: '0.0.2',
+  git: 'https://github.com/Differential/meteor-vulcanize'
 });
 
 Package.registerBuildPlugin({
