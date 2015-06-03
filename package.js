95c86d59662c76f9637da693b55d7051a912ca48
schnie <greg@differential.com>
2015-06-02 21:54:43 -0400

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'differential:vulcanize',
   summary: 'Vulcanize',
-  version: '0.0.5',
+  version: '0.0.6',
   git: 'https://github.com/Differential/meteor-vulcanize'
 });
 
