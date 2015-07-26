2ac3773228f8a8576b97ff917d33b1af43f3c819
Ry Walker <ry@rywalker.com>
2015-07-26 17:01:32 -0400

    Bump version for doc change

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'differential:vulcanize',
   summary: 'Vulcanize',
-  version: '2.0.0',
+  version: '2.0.1',
   git: 'https://github.com/Differential/meteor-vulcanize'
 });
 
